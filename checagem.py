6d698198a452701edd004da7eddbdf6db5fb6bf6
Pablo Holanda <phol4nda@gmail.com>
2015-10-27 22:52:59 -0300

    Adcionado comentários

--- checagem.py
+++ checagem.py
@@ -1,12 +1,14 @@
+# -*- coding: utf-8 -*-
 __author__ = 'pabloholanda'
 import urllib2
 from time import sleep
 
 while True:
     try:
+        # Tenta acessar a URL dentro do timeout definido em segundos
         code = urllib2.urlopen("http://127.0.0.1:8000", timeout=1).getcode()
-        print code
     except urllib2.URLError as e:
+        # Caso não consiga acessar, checamos o tipo de erro
         if str(e.reason) == 'timed out':
             print "Timed out"
         elif str(e.reason) == '[Errno 61] Connection refused':
