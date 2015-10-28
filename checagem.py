56cc34f30bf34493a5aac039ef10367a7da6a698
Pablo Holanda <phol4nda@gmail.com>
2015-10-28 12:37:14 -0300

    Criada função para tratar o control+C

--- checagem.py
+++ checagem.py
@@ -4,6 +4,8 @@ import urllib2
 from time import sleep
 import yaml
 import socket
+import sys
+import signal
 
 
 def carregar_configuracoes():
@@ -11,20 +13,28 @@ def carregar_configuracoes():
         data = yaml.load(configuracoes)
 
 
-while True:
-    try:
-        # Tenta acessar a URL dentro do timeout definido em segundos
-        code = urllib2.urlopen("http://pmaq.lais.huol.ufrn.br/amaq", timeout=1).getcode()
-    except urllib2.URLError as e:
-        # Caso não consiga acessar, checamos o tipo de erro
-        if str(e.reason) == 'timed out':
-            print "Timed out"
-        elif str(e.reason) == '[Errno 61] Connection refused':
-            print "Refused"
-    except socket.timeout as e:
-        if str(e) == 'timed out':
-            print "socket timed out"
-    except:
-        pass
+def signal_handler(signal, frame):
+    print('Você apertou Ctrl+C!')
+    sys.exit(0)
 
-    sleep(10)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+
+    while True:
+        try:
+            # Tenta acessar a URL dentro do timeout definido em segundos
+            code = urllib2.urlopen("http://pmaq.lais.huol.ufrn.br/amaq", timeout=1).getcode()
+        except urllib2.URLError as e:
+            # Caso não consiga acessar, checamos o tipo de erro
+            if str(e.reason) == 'timed out':
+                print "Timed out"
+            elif str(e.reason) == '[Errno 61] Connection refused':
+                print "Refused"
+        except socket.timeout as e:
+            if str(e) == 'timed out':
+                print "socket timed out"
+        except:
+            pass
+
+        sleep(10)
