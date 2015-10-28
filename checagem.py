dcd02e4ede93da3d08d98b47d80ba0959aaae113
Pablo Holanda <phol4nda@gmail.com>
2015-10-28 12:34:02 -0300

    Melhoria nos except

--- checagem.py
+++ checagem.py
@@ -3,6 +3,7 @@ __author__ = 'pabloholanda'
 import urllib2
 from time import sleep
 import yaml
+import socket
 
 
 def carregar_configuracoes():
@@ -10,16 +11,20 @@ def carregar_configuracoes():
         data = yaml.load(configuracoes)
 
 
-
 while True:
     try:
         # Tenta acessar a URL dentro do timeout definido em segundos
-        code = urllib2.urlopen("http://127.0.0.1:8000", timeout=1).getcode()
+        code = urllib2.urlopen("http://pmaq.lais.huol.ufrn.br/amaq", timeout=1).getcode()
     except urllib2.URLError as e:
         # Caso não consiga acessar, checamos o tipo de erro
         if str(e.reason) == 'timed out':
             print "Timed out"
         elif str(e.reason) == '[Errno 61] Connection refused':
             print "Refused"
+    except socket.timeout as e:
+        if str(e) == 'timed out':
+            print "socket timed out"
+    except:
+        pass
 
     sleep(10)
