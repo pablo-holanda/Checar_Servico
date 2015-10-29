89fb9d5b5e7a87e0c7ce01cf7986e9a8ba453404
Pablo Holanda <phol4nda@gmail.com>
2015-10-29 11:54:06 -0300

    Projeto adaptado para ser escalável.

--- checagem.py
+++ checagem.py
@@ -10,19 +10,15 @@ import signal
 
 def carregar_configuracoes():
     with open("config.yaml", "r") as configuracoes:
-        data = yaml.load(configuracoes)
+        return yaml.load(configuracoes)
 
 
-def enviar_sms(tipo_erro):
-
-    with open("config.yaml", "r") as destinatarios:
-        data = yaml.load(destinatarios)
-
+def enviar_sms(data, projeto, tipo_erro):
     for destinatario in data['destinatarios']:
         destino = '/var/spool/sms/outgoing/%s.txt' % destinatario
         arquivo = open(destino, 'a')
-        arquivo.write("To: %s \n\nOlá %s, estamos enfrentando o seguinte erro no servidor do AMAQ: %s." %
-                      (data['destinatarios'][destinatario]['telefone'], destinatario, tipo_erro))
+        arquivo.write("To: %s \n\nOlá %s, estamos enfrentando o seguinte erro no servidor do %s: %s." %
+                      (data['destinatarios'][destinatario]['telefone'], destinatario, projeto, tipo_erro))
         arquivo.close()
         sleep(0.5)
 
@@ -35,20 +31,23 @@ def signal_handler(signal, frame):
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
+    data = carregar_configuracoes()
+
     while True:
-        try:
-            # Tenta acessar a URL dentro do timeout definido em segundos
-            code = urllib2.urlopen("http://pmaq.lais.huol.ufrn.br/amaq", timeout=10).getcode()
-        except urllib2.URLError as e:
-            # Caso não consiga acessar, checamos o tipo de erro
-            if str(e.reason) == 'timed out':
-                enviar_sms(str(e.reason))
-            elif str(e.reason) == '[Errno 61] Connection refused':
-                 enviar_sms(str(e.reason))
-        except socket.timeout as e:
-            if str(e) == 'timed out':
-                 enviar_sms(str(e.reason))
-        except:
-            pass
+        for projeto in data['projetos']:
+            try:
+                # Tenta acessar a URL dentro do timeout definido em segundos
+                code = urllib2.urlopen(data['projetos'][projeto]['url'], timeout=0.1).getcode()
+            except urllib2.URLError as e:
+                # Caso não consiga acessar, checamos o tipo de erro
+                if str(e.reason) == 'timed out':
+                    enviar_sms(data['projetos'][projeto], projeto, str(e.reason))
+                elif str(e.reason) == '[Errno 61] Connection refused':
+                     enviar_sms(data['projetos'][projeto], projeto, str(e.reason))
+            except socket.timeout as e:
+                if str(e) == 'timed out':
+                     enviar_sms(data['projetos'][projeto], projeto, str(e.reason))
+            except:
+                pass
 
         sleep(60)
